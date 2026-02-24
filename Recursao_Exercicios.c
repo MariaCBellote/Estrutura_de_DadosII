7a93d977c8f5ba9023b600de4c12ba4f18222157
Maria Carla Bellote <159292487+MariaCBellote@users.noreply.github.com>
2026-02-23 22:16:38 -0300

    Update Recursao_Exercicios.c

--- Recursao_Exercicios.c
+++ Recursao_Exercicios.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 
+
+/* 1. Implemente uma fun¸c˜ao que multiplique dois inteiros positivos. */
+
 int multiplicar(int a, int b){
     if(a==0)
     return 0;
@@ -10,9 +13,42 @@ int multiplicar(int a, int b){
     
 }
 
+/* Para tratar o problema relacionado a stack overflow existem duas maneiras:
+1: Recursão em causa
+
+int multiplicar_aux(int a, int b, int resultado){
+    if(a == 0)
+        return resultado;
+    else
+        return multiplicar_aux(a-1, b, resultado + b);
+}
+
+int multiplicar(int a, int b){
+    return multiplicar_aux(a, b, 0);
+}
+
+
+
+2:Recursão eficiente
+
+int multiplicar(int a, int b){
+
+    if(a == 0)
+        return 0;
+
+    if(a % 2 == 0)
+        return multiplicar(a/2, b+b);
+
+    else
+        return b + multiplicar(a/2, b+b);
+}
+*/
+
+
+
 int main()
 {
-    printf("%d\n",multiplicar(10,4));
+    printf("%d\n",multiplicar(11,4));
 
     return 0;
 }
