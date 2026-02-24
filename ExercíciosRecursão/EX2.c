58d0056ea2f59fb579464c84d2c56cf5c3e8bffd
Maria Carla Bellote <159292487+MariaCBellote@users.noreply.github.com>
2026-02-24 20:21:10 -0300

    Update EX2.c

--- ExercíciosRecursão/EX2.c
+++ ExercíciosRecursão/EX2.c
@@ -10,13 +10,13 @@ Welcome to GDB Online.
 // Encontre o maior elemento em um vetor de n´umeros de tamanho n.
 
 #include <stdio.h>
-int MaiorElementoVetor(int vetor[], int n){
+double MaiorElementoVetor(double vetor[], int n){
 
     if(n==1){
         return vetor[0]; // quando tiver 1 elemento
     }
     
-     int maior=MaiorElementoVetor(vetor, n-1); // até ser 1 a função vai empilhando
+     double maior=MaiorElementoVetor(vetor, n-1); // até ser 1 a função vai empilhando
     
     if(vetor[n-1]>maior){ //terminando de empilhar ela compara, sempre começando com o último da pilha (vetor,1)
        return vetor[n-1]; 
@@ -30,9 +30,9 @@ int MaiorElementoVetor(int vetor[], int n){
 
 int main()
 {
-    int v[11]= {0,30,80,70,45,20,10,21,76,6,1};
+    double v[11]= {0,30.5,80.2,80.5,80.51,20,10,21,76,6,1};
     
-    printf("%d\n", MaiorElementoVetor(v,11));
+    printf("%.2f\n", MaiorElementoVetor(v,11));
 
     return 0;
 }
