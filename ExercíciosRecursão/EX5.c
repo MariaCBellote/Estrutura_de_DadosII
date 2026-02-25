70e8c651c6192684a464b8792dfaf7c6a5b00088
Maria Carla Bellote <159292487+MariaCBellote@users.noreply.github.com>
2026-02-24 21:01:02 -0300

    Update EX5.c

--- ExercíciosRecursão/EX5.c
+++ ExercíciosRecursão/EX5.c
@@ -1,1 +1,19 @@
+// Dado um n´umero n inteiro positivo, determine se ele ´e primo. Sua fun¸c˜ao deve ter complexidade O(√n).
 
+#include <stdio.h>
+
+void imprimirNumeros(int numero){
+   
+    if(numero>0){
+        imprimirNumeros(numero-1); // vai chamar a função até encher a pilha
+        printf("%d\n",numero);    // Após chegar na condição de parada é necessário desfazer a pilha. Agora os prints executam
+    }
+       
+}
+
+int main()
+{
+  imprimirNumeros(20);
+
+    return 0;
+}
