c9180c594205ca51b21a1af048e1da1fdc293f70
Maria Carla Bellote <159292487+MariaCBellote@users.noreply.github.com>
2026-02-24 23:47:37 -0300

    Update EX4.c

--- ExercíciosRecursão/EX4.c
+++ ExercíciosRecursão/EX4.c
@@ -1,16 +1,33 @@
 // Dado um n´umero n inteiro positivo, determine se ele ´e primo. Sua fun¸c˜ao deve ter complexidade O(√n).
 
-#include <stdio.h>
+// Dado um n´umero n inteiro positivo, determine se ele ´e primo. Sua fun¸c˜ao deve ter complexidade O(√n).
 
-void NumeroPrimo(int numero){
-   
-   
+#include <stdio.h>
+// Normal
+int NumeroPrimo(int numero, int divisor){
+    
+    if(numero == divisor){
+        printf("Primo");
+        return 1;
+    }
+    if(numero % divisor == 0){
+          printf("Não Primo");
+        return 0;
+    }
+    return NumeroPrimo(numero, divisor + 1);
 }
+
+int primo (int numero){
+    return NumeroPrimo(numero,2);
+}
+
+//O raiz de n ?
+
 
 int main()
 {
     
- NumeroPrimo(11);
+ primo(10);
 
     return 0;
 }
