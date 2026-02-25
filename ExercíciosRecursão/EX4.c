2efa087f830df929ecbc662bef11625ff935dee0
Maria Carla Bellote <159292487+MariaCBellote@users.noreply.github.com>
2026-02-24 23:01:47 -0300

    Update EX4.c

--- ExercíciosRecursão/EX4.c
+++ ExercíciosRecursão/EX4.c
@@ -1,1 +1,16 @@
+// Dado um n´umero n inteiro positivo, determine se ele ´e primo. Sua fun¸c˜ao deve ter complexidade O(√n).
 
+#include <stdio.h>
+
+void NumeroPrimo(int numero){
+   
+   
+}
+
+int main()
+{
+    
+ NumeroPrimo(11);
+
+    return 0;
+}
